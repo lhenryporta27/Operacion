2e89ce69ca3d5406ffdfc45c57d2541c1062e12f
lhenryporta27 <74357127@continental.edu.pe>
2025-04-13 16:49:31 -0500

    Codigo de la rama main

---
+++ Suma.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese dos números
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+# Realizar la suma
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print(f"La suma de {numero1} y {numero2} es: {suma}")
